69b7973f2116632a7d120d9060581e97d26de942
Wares <fazelyar.wares@gmail.com>
2020-02-18 01:33:34 +0000

    put in a post route to receieve form submission and implemented random string generator

--- express_server.js
+++ express_server.js
@@ -1,8 +1,22 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('view engine', 'ejs')
+
+function generateRandomString() {
+  let result = '';
+  let chars = '12346789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM'
+  
+  for (let i = 0; i <= 6; i++) {
+    let gen = Math.floor(Math.random() * (63 - 0) + 0);
+    result += chars[gen]
+  }
+return result;
+}
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -24,6 +38,19 @@ app.get("/hello", (req, res) => {
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase }
   res.render("urls_index", templateVars)
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:shortURL", (req, res) => {
