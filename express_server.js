2694af600d8b3467dfaae4810c2c2a4e067c4559
Wares <fazelyar.wares@gmail.com>
2020-02-18 21:25:38 +0000

    redirects short links to long link webpage

--- express_server.js
+++ express_server.js
@@ -42,23 +42,24 @@ app.get("/urls", (req, res) => {
 
 app.post("/urls", (req, res) => {
   console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
   let random = generateRandomString();
   urlDatabase[random]=req.body.longURL;
   console.log(urlDatabase);
+  res.redirect(`/urls/${random}`);
 });
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
-app.post("/urls/new", (req, res) => {
-  res.render("urls_new");
-});
-
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
 });
 
 app.listen(PORT, () => {
